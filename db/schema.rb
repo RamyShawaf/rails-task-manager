38af4f135afad78ceb01f05b41472ab34c5664be
Rammah Al Shawaf <shawaf.ramy@gmail.com>
2022-02-15 15:39:48 +0100

    02/15 Challenge 2 Complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_110329) do
+ActiveRecord::Schema.define(version: 2022_02_15_114345) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
